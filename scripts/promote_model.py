b8685e2fc3c6ffb864cbdddc2c6a4c443b5bddd2
Gavis33 <gagan.g.poojari@gmail.com>
2025-05-20 16:55:52 +0530

    CICD run 06

---
+++ scripts/promote_model.py
@@ -0,0 +1,36 @@
+import os
+import mlflow
+
+def promote_model():
+    dagshub_token = os.getenv("PROJECT2_TEST")
+    if not dagshub_token:
+        raise EnvironmentError('Dagshub token not found. Please set the "PROJECT2_TEST" environment variable.')
+
+    os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+    os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+    mlflow.set_tracking_uri('https://dagshub.com/Gavis33/project2-MLOps.mlflow')
+
+    client = mlflow.MlflowClient()
+    model_name = 'project2_model'
+
+    latest_version_in_staging = client.get_latest_versions(model_name, stages=["Staging"])[0].version
+
+    production_versions = client.get_latest_versions(model_name, stages=["Production"])
+    for production_version in production_versions:
+        client.transition_model_version_stage(
+            name=model_name,
+            version=production_version.version,
+            stage="Archived"
+        )
+
+    client.transition_model_version_stage(
+        model_name=model_name,
+        version=latest_version_in_staging,
+        stage="Production"
+        )
+
+    print(f'Model version {latest_version_in_staging} promoted to Production')
+
+if __name__ == "__main__":
+    promote_model()
---
+++ tests/text_model.py
@@ -0,0 +1,73 @@
+import os
+import mlflow
+import pickle
+import unittest
+import pandas as pd
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+class TestModelLoading(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        dagshub_token = os.getenv("PROJECT2_TEST")
+        if not dagshub_token:
+            raise EnvironmentError('Dagshub token not found. Please set the "PROJECT2_TEST" environment variable.')
+        
+        os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+        os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+        mlflow.set_tracking_uri('https://dagshub.com/Gavis33/project2-MLOps.mlflow')
+
+        cls.new_model_name = 'project2_model'
+        cls.new_model_version = cls.client.get_latest_versions(cls.new_model_name)
+        cls.new_model_uri = f"models:/{cls.new_model_name}/{cls.new_model_version}"
+        cls.new_model = mlflow.pyfunc.load_model(cls.new_model_uri)
+
+        cls.vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
+
+        cls.holdout_data = pd.read_csv('data/processed/test_bow.csv')
+
+    @staticmethod
+    def get_latest_model_version(model_name, stage="Staging"):
+        client = mlflow.MlflowClient()
+        latest_version = client.get_latest_versions(model_name, stages=[stage])
+        return latest_version[0].version if latest_version else None
+    
+    def test_model_loading(self):
+        self.assertIsNotNone(self.new_model)
+
+    def test_model_signature(self):
+        input_text = "this is a test text"
+        input_data = self.vectorizer.transform([input_text])
+        input_df = pd.DataFrame(input_data.toarray(), columns=[str(i) for i in range(input_data.shape[1])])
+
+        prediction = self.new_model.predict(input_df)
+
+        self.assertEqual(input_df.shape[1], len(self.vectorizer.get_feature_names_out()))
+
+        self.assertEqual(len(prediction), input_df.shape[0])
+        self.assertEqual(len(prediction), 1)
+
+    def test_model_performance(self):
+        X_holdout = self.holdout_data.iloc[:, 0:-1] # select all columns except the last one where iloc means index location
+        y_holdout = self.holdout_data.iloc[:, -1]
+
+        y_pred_new = self.new_model.predict(X_holdout)
+
+        accuracy_new = accuracy_score(y_holdout, y_pred_new)
+        precision_new = precision_score(y_holdout, y_pred_new)
+        recall_new = recall_score(y_holdout, y_pred_new)
+        f1_new = f1_score(y_holdout, y_pred_new)
+
+        expected_accuracy = 0.40
+        expected_precision = 0.40
+        expected_recall = 0.40
+        expected_f1 = 0.40
+
+        self.assertGreaterEqual(accuracy_new, expected_accuracy, f'Accuracy should be at least {expected_accuracy}')
+        self.assertGreaterEqual(precision_new, expected_precision, f'Precision should be at least {expected_precision}')
+        self.assertGreaterEqual(recall_new, expected_recall, f'Recall should be at least {expected_recall}')
+        self.assertGreaterEqual(f1_new, expected_f1, f'F1 score should be at least {expected_f1}')
+
+if __name__ == "__main__":
+    unittest.main()
